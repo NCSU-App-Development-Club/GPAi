3781eba746fb8e8e6c52511795e483d3e277b54a
Xavier Jones <xdjones@ncsu.edu>
2024-09-24 12:55:51 -0400

    Remove duplicate project files

--- app/src/main/java/com/adc/gpai/ui/theme/Color.kt
+++ app/src/main/java/com/adc/gpai/ui/theme/Color.kt
@@ -7,11 +7,3 @@ val BrandPurple = Color(0xFF7876ED)
 val BrandDarkPurple =Color(0xFF382540)
 val BrandSuccessGreen = Color(0xFF7DC172)
 val BrandFailureRed = Color(0xFFF34E4E)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
