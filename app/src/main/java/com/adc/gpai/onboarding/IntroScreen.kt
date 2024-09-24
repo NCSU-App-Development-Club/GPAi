2ea9bc3ef14126fc7ba57c75da9d4cf58f92c6d4
Koray Öztürkler <kerimkoray.ozturkler1@gmail.com>
2024-09-24 19:36:14 -0400

    implemented intro screen

--- app/src/main/java/com/adc/gpai/onboarding/IntroScreen.kt
+++ app/src/main/java/com/adc/gpai/onboarding/IntroScreen.kt
@@ -1,34 +1,77 @@
 package com.adc.gpai.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.adc.gpai.R
 import com.adc.gpai.ui.theme.GPAiTheme
 
 @Composable
-fun IntroScreen(navController: NavHostController? = null) {
-    Column(
+fun IntroScreen() {
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .testTag("intro_screen")
-        ,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(16.dp),
     ) {
-        Text(text = "Introduction")
-        Button(onClick = { navController?.navigate("upload") }) {
-            Text(text = "Next")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.a_plus_graphic),
+                contentDescription = "Intro Image",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Get quick academic advice with GPAi.",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Upload your transcript and ask any question you can think of " +
+                        "relating to your academic goals!",
+                style = MaterialTheme.typography.headlineSmall,
+                textAlign = TextAlign.Center
+            )
+        }
+        Button(
+            onClick = { /* Navigate to next screen */ },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Text(
+                text = "Get Started",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
