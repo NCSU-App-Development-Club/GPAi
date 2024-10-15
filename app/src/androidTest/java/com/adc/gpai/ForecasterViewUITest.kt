cd6b3f210009c193c8bc0a7ec36586a2e151309d
Kerneep Singh Sandhu <kssandhu@ncsu.edu>
2024-10-14 21:59:23 -0400

    Adding comments for scenario planning

--- app/src/androidTest/java/com/adc/gpai/ForecasterViewUITest.kt
+++ app/src/androidTest/java/com/adc/gpai/ForecasterViewUITest.kt
@@ -10,4 +10,17 @@ package com.adc.gpai
 @RunWith(AndroidJUnit4::class)
 class ForecasterViewUITest {
 
+    //scenarios to test
+//    course added successfully
+//    course details that were added match what is displayed
+//    slider has options for all grades and their relative positions are as expected
+//    (for example - A+ has to be completely on the right)
+//    cumulative and semester gpas are calculated correctly
+//        - after adding course
+//        - after removing course
+//        - having only 1 unit courses
+//        - having only 3 unit courses
+//        - having multi-unit courses
+//    course removed successfully
+
 }
