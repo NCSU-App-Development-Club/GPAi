936007710bb9cf27e70b3517c28d3cd429f4fe1c
Kerneep Singh Sandhu <kssandhu@ncsu.edu>
2024-10-14 21:49:25 -0400

    Added ForecasterViewUITest class

---
+++ app/src/androidTest/java/com/adc/gpai/ForecasterViewUITest.kt
@@ -0,0 +1,13 @@
+package com.adc.gpai
+
+/**
+ * This class contains UI tests for the Forecaster View in the app.
+ * It tests the functionality of creating new courses, setting their grades and course units
+ * and then calculating the Cumulative and Semester gpa
+ *
+ * @author Kerneep Sandhu
+ */
+@RunWith(AndroidJUnit4::class)
+class ForecasterViewUITest {
+
+}
