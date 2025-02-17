c3e32fc6e2d6097087f55dbaac20115c4430021e
Peter Ising <pmising@ncsu.edu>
2025-02-17 15:22:51 -0500

    Creating Forecaster Unit testing file

---
+++ app/src/test/java/com/adc/gpai/ForecasterUnitTests.kt
@@ -0,0 +1,52 @@
+package com.adc.gpai
+
+import com.adc.gpai.utils.PDFUtils
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Unit tests for the Forecaster functionality, such as GPA calculations
+ */
+class ForecasterUnitTests {
+
+    /**
+     *
+     */
+    @Test
+    fun testGPACalcValid() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    fun testGPACalcNoInput() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    fun testGPACalcInvalidInputs() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    fun testGPACalcBoundCases() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    fun testGPACalcRounding() {
+
+    }
+}
